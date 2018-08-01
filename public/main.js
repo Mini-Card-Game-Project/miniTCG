988d7a5c5dd3fcdf7642d128c5e605c7d0592c35
Your Name <fahrisyany@gmail.com>
2018-08-01 14:03:10 +0700

    add view

--- public/main.js
+++ public/main.js
@@ -1,12 +1,10 @@
     // Get the modal
     var modal = document.getElementById('myModal');
     
-alert ("modal "+modal );
 
     // Get the button that opens the modal
     var btn = document.getElementById("myBtn");
 
-    alert( btn );
     
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
