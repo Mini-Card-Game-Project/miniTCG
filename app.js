4cb829e81697e9a886e5a73bedac7f487e90bcb3
hacktiv8 <hacktiv8@H8JKT004.localdomain>
2018-08-01 13:45:01 +0700

    added routes and partials

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const routes = require ('./routes')
+const path = require ('path');
+
+app.set ('view engine', 'ejs')
+app.set ('views', path.join(__dirname, 'views'))
+
+app.use (express.json());
+app.use (express.urlencoded({extended:false}));
+app.use ('/public', express.static(path.join(__dirname + '/public')))
+
+// app.use(session({
+  //   secret: 'hello world',
+  //   resave: false,
+  //   saveUninitialized: true,
+  // }))
+
+app.locals.isLoggedIn = require ('./helpers/isLoggedIn');
+  
+app.use('/', routes);
+
+
+app.listen(3000, ()=> {
+  console.log('listening to 3000')
+})
