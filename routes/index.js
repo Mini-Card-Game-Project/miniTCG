0ab72a4daf5e73b4c4ffa5fe99ca41a346ef069b
kneric <brian.pradipta.a@gmail.com>
2018-08-03 02:52:41 +0700

    class & instance method

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 const model = require('../models');
-const bcrypt = require('bcrypt');
 const account = require('./account');
 const deckList = require('./deckList');
 const payment = require('./payment');
 const registration = require('./registration');
 const lobby = require('./lobby');
-const admin = require ('./admin')
 const battle = require('./battle');
 const isLoggedIn = require ('../helpers/isLoggedIn');
 
@@ -15,35 +13,23 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res)=> {
+  console.log(req.body)
   model.Player
-  .find({
-    where : {
-      username: req.body.username,
-    }
-  })
-  .then(player=>{
-    if (player){
-      bcrypt.compare(req.body.password, player.password, function(err, result) {
-        if (err){
-          throw err;
-        }
-        if (result){
-          req.session.username = req.body.username;
-          res.redirect('/account')
-        } else {
-          res.render('home/index', {errors: [{message: 'wrong password'}]})
-        }
-    });
+  .loginCheck(req.body.username, req.body.password, (result) => {
+    if (result != null){
+      if(result == true){
+        req.session.username = req.body.username;
+        res.redirect('/account')
+      } else {
+        res.render('home/index', {errors: [{message: 'wrong password'}]})
+      }
     } else {
       res.render('home/index', {errors: [{message: 'Username not found, register first!'}]})
     }
   })
-  .catch(err => {
-    console.log(err);
-  })
 })
 
-router.use('/adminnih', admin)
+
 router.use('/account', isLoggedIn, account);
 router.use('/decklist', isLoggedIn, deckList);
 router.use('/payment', isLoggedIn, payment);
